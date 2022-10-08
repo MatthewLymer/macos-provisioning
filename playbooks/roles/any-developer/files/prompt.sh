4b41857086fc7955cbe3967b2da2265ce5ca86d8
Matthew Lymer <matthew@lymer.ca>
2022-10-07 21:04:40 -0400

    Update macos prompt

--- playbooks/roles/any-developer/files/prompt.sh
+++ playbooks/roles/any-developer/files/prompt.sh
@@ -3,34 +3,70 @@
 # 
 
 COLOR_DEFAULT='%f'
-COLOR_DIRECTORY='%F{197}'
-COLOR_GIT='%F{39}'
 
-parse_git_branch() {
-    git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/\1/p'
+COLOR_DIRECTORY='%F{197}'
+
+COLOR_GIT='%F{39}'
+COLOR_MODIFIED='%F{36}'
+COLOR_AHEAD='%F{046}'
+COLOR_BEHIND='%F{226}'
+COLOR_NO_UPSTREAM='%F{196}'
+
+get_git_branch() {
+    git branch --show-current 2>/dev/null
 }
 
-parse_git_status() {
+get_uncommitted_file_count() {
     git status --short 2> /dev/null | wc -l | tr -d ' '
 }
 
+get_upstream() {
+    git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null
+}
+
+get_ahead_behind_counts() {
+    git rev-list --count --left-right head...$(get_upstream) 2>/dev/null
+}
+
 format_git_prompt() {
-    BRANCH=$(parse_git_branch)
+    BRANCH=$(get_git_branch)
     if [[ "${BRANCH}" = "" ]] 
     then
         return
     fi
 
-    STATUS=$(parse_git_status)
+    OUTPUT="${BRANCH}"
 
-    if [[ "${STATUS}" = "0" ]]
+    UNCOMMITED_FILE_COUNT=$(get_uncommitted_file_count)
+
+    if [[ "${UNCOMMITED_FILE_COUNT}" != "0" ]]
     then
-        echo "[${BRANCH}] "
-    else
-        echo "[${BRANCH} Δ ${STATUS}]"
+        OUTPUT="${OUTPUT} ${COLOR_MODIFIED}~${UNCOMMITED_FILE_COUNT}${COLOR_GIT}"
     fi
+
+    UPSTREAM=$(get_upstream)
+
+    if [[ "${UPSTREAM}" = "" ]]
+    then
+        OUTPUT="${OUTPUT} ${COLOR_NO_UPSTREAM}x${COLOR_GIT}"
+    else
+        AHEAD_BEHIND_COUNTS=$(get_ahead_behind_counts)
+        AHEAD=$(echo ${AHEAD_BEHIND_COUNTS} | awk '{print $1}')
+        if [[ "${AHEAD}" != "0" ]]
+        then
+            OUTPUT="${OUTPUT} ${COLOR_AHEAD}↑${AHEAD}${COLOR_GIT}"
+        fi
+
+        BEHIND=$(echo ${AHEAD_BEHIND_COUNTS} | awk '{print $2}')
+        if [[ "${BEHIND}" != "0" ]]
+        then
+            OUTPUT="${OUTPUT} ${COLOR_BEHIND}↓${BEHIND}${COLOR_GIT}"
+        fi
+    fi
+
+    echo "${COLOR_GIT}[${OUTPUT}]${COLOR_DEFAULT}"
 }
 
 setopt PROMPT_SUBST
 
-export PROMPT='${COLOR_DIRECTORY}%d ${COLOR_GIT}$(format_git_prompt)${COLOR_DEFAULT}%% '
+export PROMPT='${COLOR_DIRECTORY}%d${COLOR_DEFAULT} $(format_git_prompt)%% '
